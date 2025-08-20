869f09ac23e3bb7a6b634f0a59039ffab1c9f1f5
Trantoan12022004 <tranvantoan.work@gmail.com>
2025-08-20 20:34:38 +0700

    fixx cors2

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const corsOptions = {
                   "http://localhost:5500",
                   "http://127.0.0.1:3000",
                   "http://.0.1270.1:5173",
+                  "http://127.0.0.1:5500",
               ],
     credentials: true,
     optionsSuccessStatus: 200,
